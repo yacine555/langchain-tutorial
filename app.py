3d54ff951a99ddeb3ce13e936c83e03f7f30d090
Yacine <ybouakkaz@gmail.com>
2025-05-05 17:30:33 -0400

    add prompt

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import getpass
 import os
+from langchain.chat_models import init_chat_model
+from langchain_core.prompts import ChatPromptTemplate
 
 
 try:
@@ -25,7 +27,7 @@ if "LANGSMITH_PROJECT" not in os.environ:
 if not os.environ.get("ANTHROPIC_API_KEY"):
   os.environ["ANTHROPIC_API_KEY"] = getpass.getpass("Enter API key for Anthropic: ")
 
-from langchain.chat_models import init_chat_model
+
 
 model = init_chat_model("claude-3-5-sonnet-latest", model_provider="anthropic")
 
@@ -38,5 +40,19 @@ messages = [
 
 model.invoke(messages)
 
-for token in model.stream(messages):
-    print(token.content, end="|")
+# for token in model.stream(messages):
+#     print(token.content, end="|")
+
+
+
+
+system_template = "Translate the following from English into {language}"
+
+prompt_template = ChatPromptTemplate.from_messages(
+    [("system", system_template), ("user", "{text}")]
+)
+
+prompt = prompt_template.invoke({"language": "German", "text": "Good morning!"})
+
+response = model.invoke(prompt)
+print(response.content)
